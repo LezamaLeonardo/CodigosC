ea1d5c167682e629ad22c921f9ae606f552dcf8f
Niluxfg <109265347+Niluxfg@users.noreply.github.com>
2024-05-13 12:27:08 -0600

    Actualizar archivo15.c

--- archivo15.c
+++ archivo15.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
+void ventaProductos() {
+    int cantidad;
+    float precio;
+    float Importe=0;
+
+    printf("Dime la cantidad de productos: ");
+    scanf("%d", &cantidad);
+
+    while (cantidad > 0){
+        if (cantidad <= 0){
+            printf("La cantidad debe ser positiva\n");
+            continue;
+        }
+        printf("Dime el precio del producto: ");
+        scanf("%f", &precio);
+
+        if (precio <= 0){
+            printf("El precio debe ser positivo\n");
+            continue;
+        }
+
+        Importe += cantidad * precio;
+        printf("Tu importe actual es de: %.2f\n", Importe);
+        printf("Dime la cantidad de productos (0 para terminar): ");
+        scanf("%d", &cantidad);
+    }
+}
+
 int main(){
     char usuario[20];
     char contra[20];
@@ -27,34 +55,8 @@ int main(){
         }
     }
 
-   
     if (intentos > 0) {
-        int cantidad;
-        float precio;
-        float Importe=0;
-
-        printf("Dime la cantidad de productos: ");
-        scanf("%d", &cantidad);
-
-        while (cantidad > 0){
-            if (cantidad <= 0){
-                printf("La cantidad debe ser positiva\n");
-                continue;
-            }
-            printf("Dime el precio del producto: ");
-            scanf("%f", &precio);
-
-            if (precio <= 0){
-                printf("El precio debe ser positivo\n");
-                continue;
-            }
-
-          
-            Importe += cantidad * precio;
-            printf("Tu importe actual es de: %.2f\n", Importe);
-            printf("Dime la cantidad de productos (0 para terminar): ");
-            scanf("%d", &cantidad);
-        }
+        ventaProductos();
     }
 
     return 0;
